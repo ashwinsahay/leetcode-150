081caab57a388fad7ea0098a07bd58a85383ddc6
ashwinsahay <ashwin.sahay@fox.com>
2024-04-23 00:42:46 +0530

    adding description for merge sorted array problem

--- problems/Merger_sorted_array.py
+++ problems/Merger_sorted_array.py
@@ -8,14 +8,47 @@ Merge nums1 and nums2 into a single array sorted in non-decreasing order.
 
 The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
 
-Example 1 - 
+ 
+
+Example 1:
 
 Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
 Output: [1,2,2,3,5,6]
 Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
 The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
+Example 2:
+
+Input: nums1 = [1], m = 1, nums2 = [], n = 0
+Output: [1]
+Explanation: The arrays we are merging are [1] and [].
+The result of the merge is [1].
+Example 3:
+
+Input: nums1 = [0], m = 0, nums2 = [1], n = 1
+Output: [1]
+Explanation: The arrays we are merging are [] and [1].
+The result of the merge is [1].
+Note that because m = 0, there are no elements in nums1. The 0 is only there to ensure the merge result can fit in nums1.
 
 '''
+
+
+
+"""
+
+Use Pointers: Since both nums1 and nums2 are already sorted, you can use three pointers to efficiently merge them:
+i pointing to the last element in the initial part of nums1 that contains data (i.e., m-1).
+j pointing to the last element in nums2 (i.e., n-1).
+k pointing to the last position in nums1 where the next element should be placed (i.e., m+n-1).
+Merge in Reverse Order:
+Compare elements at nums1[i] and nums2[j].
+Place the larger of the two at nums1[k] and decrement the respective pointers (i, j, k).
+Continue this process until all elements of one array are placed.
+Handle Remaining Elements:
+If elements remain in nums2 (i.e., j >= 0), they need to be copied into nums1. This step is required because the remaining elements in nums1 are already in their correct position if i >= 0.
+
+
+"""
 
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
